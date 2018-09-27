29cd16071af420d0acd2a58c5be86e2f72e221d4
Superman <703353105@qq.com>
2018-09-27 10:54:54 +0800

    修改代码

--- AlipayHomePageDemo/ViewController.m
+++ AlipayHomePageDemo/ViewController.m
@@ -7,16 +7,45 @@
 //
 
 #import "ViewController.h"
+//#import "MJRefresh.h"
+#import <MJRefresh/MJRefresh.h>
 
-@interface ViewController ()
+#define kWidth [UIScreen mainScreen].bounds.size.width
+#define kHeight [UIScreen mainScreen].bounds.size.height
+
+
+#define functionHeaderViewHeight 95
+#define singleAppHeaderViewHeight 60
+
+@interface ViewController ()<UIScrollViewDelegate,UIGestureRecognizerDelegate>
+@property(strong,nonatomic)UIScrollView *mainScrollView;
+@property(strong,nonatomic)UIView *navView;
 
 @end
 
 @implementation ViewController
+-(UIScrollView *)mainScrollView{
+    if (!_mainScrollView) {
+        CGFloat height = kHeight -64;
+        _mainScrollView=[[UIScrollView alloc]initWithFrame:CGRectMake(0, 64, kWidth, height)];
+        _mainScrollView.delegate=self;
+        _mainScrollView.contentSize = CGSizeMake(kWidth,100);
+        _mainScrollView.scrollIndicatorInsets = UIEdgeInsetsMake(155, 0, 0, 0);
+    }
+    return _mainScrollView;
+}
+-(UIView *)navView{
+    if (!_navView) {
+        _navView=[[UIView alloc]initWithFrame:CGRectMake(0, 0, kWidth, 64)];
+        _navView.backgroundColor=[UIColor colorWithRed:65/255.0 green:128/255.0 blue:1 alpha:1];
+    }
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    int topOffsetY =functionHeaderViewHeight + singleAppHeaderViewHeight;
+    
 }
 
 
@@ -27,3 +56,26 @@
 
 
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
